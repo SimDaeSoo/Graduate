20f71a8b4d32e4364d2c87d0e42df3c96a81105a
SIMDAESOO\SimDaeSoo <tlaeotn123@naver.com>
2018-03-21 13:32:35 +0900

    오류 디버그.

--- app.js
+++ app.js
@@ -173,7 +173,7 @@ app.post('/message', function(req,res){
                   }
                 }
                 res.send(answer);
-              }
+              });
             }else{
 
               if(system_mode == 1){
