13705bfbca930fd1b1e2fb8980383f9ebe3def32
daesoo94 <daesoo94@nexon.com>
2018-11-11 15:22:25 +0900

    no message

--- app.js
+++ app.js
@@ -183,7 +183,7 @@ app.post('/message', function(req,res){
   console.log(encoded);
   var url = encoded;
   horseman.open(url)
-  .crop({left: 200, top: 300, right: 800, bottom: 600}, __dirname+'/images/img.JPEG');
+  .crop({left: '135', top: '200', right: '620', bottom: '315'}, __dirname+'/images/img.JPEG');
 
   sleep(1000);
 
@@ -490,8 +490,8 @@ app.post('/message', function(req,res){
                       "text":Answer_tbl[Similarity_Q_Id].answer + "\n[유사도 : " +level+ "]", // in case 'text'
                       "photo": {
                         "url": "http://13.125.224.92:8080/images/img.jpeg",
-                        "width": 600,
-                        "height": 300
+                        "width": 485,
+                        "height": 115
                       }
                     }
                   }
